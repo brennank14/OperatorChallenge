67fcd0e03fa7a6c848c8b2a0476c1934472b1a9c
brennank14 <brennank14@gmail.com>
2020-10-29 16:48:17 -0500

    challenge complete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    double firstDouble = 20.00;
+	    double secondDouble = 80.00;
+        double stepThree = (firstDouble + secondDouble) * 10;
+
+        double stepFour = stepThree % 40.00;
+        boolean stepFive = stepFour == 0 ? true : false;
+
+        System.out.println(stepFive);
+
+        if (!stepFive) {
+            System.out.println("Got some remainder");
+        }
     }
 }
